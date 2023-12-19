2d7ec025eb09e64a8e0c2b046900b6841cbe9290
Naus4816 <128583047+Naus4816@users.noreply.github.com>
2023-12-19 11:23:22 +0100

    Update Stats_goal.kt

--- app/src/main/java/com/example/myapplication/Stats_goal.kt
+++ app/src/main/java/com/example/myapplication/Stats_goal.kt
@@ -1,10 +1,8 @@
 package com.example.myapplication
 
-class Stats_goal(val id_joueur: Int,
-                 var degagement: Int,
+class Stats_goal(var degagement: Int,
                  var jeu_de_main: Int,
                  var plongeon: Int,
                  var reflexes: Int,
                  var vitesse: Int,
-                 val id: Int
 )
