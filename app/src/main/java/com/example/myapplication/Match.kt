1fd8432a72f28d1e867d7b9ea22b9415b0d9c128
Naus4816 <128583047+Naus4816@users.noreply.github.com>
2023-12-19 11:22:16 +0100

    Update Match.kt

--- app/src/main/java/com/example/myapplication/Match.kt
+++ app/src/main/java/com/example/myapplication/Match.kt
@@ -6,98 +6,189 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.Tasks
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
 import kotlin.random.Random
 
-class Match(val equipe1: String, val equipe2: String, var score1: Int, var score2: Int, val id: Int)
+class Match(
+    val id: String,
+    val equipe1: String,
+    val equipe2: String,
+    var score1: Int,
+    var score2: Int,
+    var tour_match: Int
+)
 
 class MatchFragment : Fragment(R.layout.fragment_match) {
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val equipe1TextView: TextView = view.findViewById(R.id.Equipe1)
         val equipe2TextView: TextView = view.findViewById(R.id.Equipe2)
         val scoreTextView: TextView = view.findViewById(R.id.Score)
-
-        // TODO: Charger les valeurs des notes des équipes depuis la base de données
-        val noteEquipe1 = 89
-        val noteEquipe2 = 90
-
-        val match = Match("Equipe 1", "Equipe 2", 0, 0, 1)
-
-        equipe1TextView.text = match.equipe1
-        equipe2TextView.text = match.equipe2
-
-
-        val myApp = requireActivity().application as MyApplication
-        var joursRestants = myApp.joursRestants
-        var tour = myApp.tour
-
-
-
         val btnMatch: Button = view.findViewById(R.id.btnMatch) ?: return
 
-            btnMatch.isEnabled = joursRestants == 0
+        val myApp = requireActivity().application as MyApplication
+        var joursRestants = myApp.Data.joursRestants
+        var tour = myApp.Data.tour
 
-            btnMatch.visibility = View.VISIBLE
-            btnMatch.setOnClickListener {
-                // Comparer les notes et attribuer les scores en conséquence
-                var score1: Int
-                var score2: Int
+        val equipeChoisieCollection =
+            FirebaseFirestore.getInstance().collection("equipe_choisie")
 
-                if (noteEquipe1 > noteEquipe2) {
-                    score1 = Random.nextInt(1, 11)
-                    score2 = Random.nextInt(1, score1)
-                } else {
-                    score2 = Random.nextInt(1, 11)
-                    score1 = Random.nextInt(1, score2)
-                }
+        equipeChoisieCollection.get().addOnSuccessListener { equipeSnapshot ->
+            if (!equipeSnapshot.isEmpty) {
+                val equipeDocument = equipeSnapshot.documents[0]
+                val equipeChoisie = Equipe(
+                    equipeDocument.getString("nom") ?: "",
+                    equipeDocument.id
+                )
+                Log.d("equipechoisie","equipechoisie:${equipeChoisie.id}")
 
-                // S'assurer que les scores ne sont pas égaux, entiers positifs et inférieurs à 10
-                while (score1 == score2 || score1 >= 10 || score2 >= 10) {
-                    score1 = Random.nextInt(1, 11)
-                    score2 = Random.nextInt(1, 11)
-                }
+                val matchsCollection =
+                    FirebaseFirestore.getInstance().collection("match")
 
-                if (score1 != 0 || score2 != 0) {
-                    scoreTextView.text = "        $score1:$score2"
-                }
-                else {
-                    scoreTextView.visibility = View.GONE
-                }
-                if (score1 > score2) {
-                    scoreTextView.append("\nWIN")
-                    tour ++
-                    myApp.updateTour(tour)
-                    Log.d("tour", "tour $tour")
+                val matchQueryEquipe1 =
+                    matchsCollection.whereEqualTo("equipe1", equipeChoisie.nom)
+                val matchQueryEquipe2 =
+                    matchsCollection.whereEqualTo("equipe2", equipeChoisie.nom)
 
-                    // Si c'est la finale, démarrer le fragment des champions
-                    if (tour == 3) {
-                        tour = 0
-                        myApp.updateTour(tour)
-                        val championsFragment = ChampionsFragment()
+                val combinedQuery: Task<List<QuerySnapshot>> =
+                    Tasks.whenAllSuccess(matchQueryEquipe1.get(), matchQueryEquipe2.get())
 
-                        // Remplacez le fragment actuel par le fragment des champions
-                        val transaction = requireActivity().supportFragmentManager.beginTransaction()
-                        transaction.replace(R.id.mainActivity, championsFragment)
-                        transaction.addToBackStack(null)
-                        transaction.commit()
+                var match: Match? = null
+
+                combinedQuery.addOnSuccessListener { taskSnapshots ->
+                    val matchSnapshotEquipe1 = taskSnapshots[0] as QuerySnapshot
+                    val matchSnapshotEquipe2 = taskSnapshots[1] as QuerySnapshot
+
+                    if (!matchSnapshotEquipe1.isEmpty || !matchSnapshotEquipe2.isEmpty) {
+                        // Au moins une des deux requêtes a retourné des résultats
+                        // Utilisez les résultats comme nécessaire
+                        if (!matchSnapshotEquipe1.isEmpty) {
+                            // L'équipe est dans la position equipe1
+                            val matchDocument = matchSnapshotEquipe1.documents[0]
+                             match = Match(
+                                matchDocument.id,
+                                matchDocument.getString("equipe1") ?: "",
+                                matchDocument.getString("equipe2") ?: "",
+                                matchDocument.getLong("score1")?.toInt() ?: 0,
+                                matchDocument.getLong("score2")?.toInt() ?: 0,
+                                matchDocument.getLong("tour")?.toInt() ?: 0
+                            )
+
+                            // Faites quelque chose avec l'objet Match...
+
+                        } else {
+                            // L'équipe est dans la position equipe2
+                            val matchDocument = matchSnapshotEquipe2.documents[0]
+                             match = Match(
+                                matchDocument.id,
+                                matchDocument.getString("equipe1") ?: "",
+                                matchDocument.getString("equipe2") ?: "",
+                                matchDocument.getLong("score1")?.toInt() ?: 0,
+                                matchDocument.getLong("score2")?.toInt() ?: 0,
+                                matchDocument.getLong("tour")?.toInt() ?: 0
+                            )
+
+                            // Faites quelque chose avec l'objet Match...
+
+                        }
+                        equipe1TextView.text = match!!.equipe1
+                        equipe2TextView.text = match!!.equipe2
+                        btnMatch.isEnabled = joursRestants == 0
+                        btnMatch.visibility = View.VISIBLE
+
+                        btnMatch.setOnClickListener {
+                            var score1: Int
+                            var score2: Int
+                            val nom = match!!.equipe1
+                            val db = FirebaseFirestore.getInstance()
+                            val equipeCollection = db.collection("equipe")
+                            // Récupérer les notes des équipes à partir de la collection "equipe"
+                            val equipe1NoteQuery = equipeCollection.whereEqualTo("nom", nom)
+                            Log.d("Snapshot","Snapshot:$nom")
+                            equipe1NoteQuery.get().addOnSuccessListener { equipe1Snapshot ->
+                                if (!equipe1Snapshot.isEmpty) {
+                                    Log.d("note","note")
+                                    val noteEquipe1 = equipe1Snapshot.documents[0].getLong("note")?.toInt() ?: 0
+
+                                    val equipe2NoteQuery = equipeCollection.whereEqualTo("nom", nom)
+                                    equipe2NoteQuery.get().addOnSuccessListener { equipe2Snapshot ->
+                                        if (!equipe2Snapshot.isEmpty) {
+                                            val noteEquipe2 = equipe2Snapshot.documents[0].getLong("note")?.toInt() ?: 0
+
+                                            // Utiliser la note de l'équipe2
+                                            // ...
+
+                                            if (noteEquipe1 > noteEquipe2) {
+                                                score1 = Random.nextInt(1, 11)
+                                                score2 = Random.nextInt(1, score1)
+                                            } else {
+                                                score2 = Random.nextInt(1, 11)
+                                                score1 = Random.nextInt(1, score2)
+                                            }
+
+                                            while (score1 == score2 || score1 >= 10 || score2 >= 10) {
+                                                score1 = Random.nextInt(1, 11)
+                                                score2 = Random.nextInt(1, 11)
+                                            }
+
+                                            if (score1 != 0 || score2 != 0) {
+                                                scoreTextView.text = "        $score1:$score2"
+                                            } else {
+                                                scoreTextView.visibility = View.GONE
+                                            }
+
+                                            if (score1 > score2) {
+                                                scoreTextView.append("\nWIN")
+                                                tour++
+                                                myApp.Data.updateTour(tour)
+
+                                                if (tour == 3) {
+                                                    tour = 0
+                                                    myApp.Data.updateTour(tour)
+                                                    val championsFragment = ChampionsFragment()
+                                                    val transaction =
+                                                        requireActivity().supportFragmentManager.beginTransaction()
+                                                    transaction.replace(
+                                                        R.id.mainActivity,
+                                                        championsFragment
+                                                    )
+                                                    transaction.addToBackStack(null)
+                                                    transaction.commit()
+                                                }
+                                            } else {
+                                                val looseFragment = LooseFragment()
+                                                val transaction =
+                                                    requireActivity().supportFragmentManager.beginTransaction()
+                                                transaction.replace(
+                                                    R.id.mainActivity,
+                                                    looseFragment
+                                                )
+                                                transaction.addToBackStack(null)
+                                                transaction.commit()
+                                            }
+
+                                            myApp.Data.updateJoursRestants(3)
+                                            joursRestants = myApp.Data.joursRestants
+                                            tour = myApp.Data.tour
+                                            btnMatch.isEnabled = joursRestants == 0
+                                        }
+                                    }
+                                } else {
+                                    Log.d("Firestore", "Aucun match trouvé pour l'équipe choisie")
+                                }
+                            }
+                        }
                     }
-                } else {
-                    // Si l'équipe 1 perd, démarrer le fragment de défaite
-                    val looseFragment = LooseFragment()
-
-                    // Remplacez le fragment actuel par le fragment de défaite
-                    val transaction = requireActivity().supportFragmentManager.beginTransaction()
-                    transaction.replace(R.id.mainActivity, looseFragment)
-                    transaction.addToBackStack(null)
-                    transaction.commit()
+                    else {
+                            Log.d("Firestore", "Aucune équipe choisie trouvée dans la collection equipe_choisie")
+                            }
+                        }
+                    }
                 }
-
-                myApp.updateJoursRestants(3)
-                joursRestants = myApp.joursRestants
-                tour = myApp.tour
-                btnMatch.isEnabled = joursRestants == 0
             }
-    }
-
-}
+        }
