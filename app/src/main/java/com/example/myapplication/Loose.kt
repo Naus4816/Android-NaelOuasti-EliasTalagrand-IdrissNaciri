7b25cff239b8ed17cd7fce2fcc557ace5128267b
Naus4816 <128583047+Naus4816@users.noreply.github.com>
2023-12-19 11:24:52 +0100

    Update Loose.kt

--- app/src/main/java/com/example/myapplication/Loose.kt
+++ app/src/main/java/com/example/myapplication/Loose.kt
@@ -12,7 +12,6 @@ class LooseFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_loose, container, false)
     }
 }
